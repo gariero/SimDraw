40ee3e56bdbcd9ab08af930646cc2b8f4bb39d8f
gariero <ceneise@live.ru>
2023-01-13 02:24:24 +0700

    14

--- js/tweaks.js
+++ js/tweaks.js
@@ -1,7 +1,7 @@
 function ChangeDropdowns(value) {
-    if (value == "los") {
+    if (value === "Round bar" || value === "Disc") {
         document.getElementById('shapeSelect').style.display = 'none';
-    } else if (value === "Round bar" || value === "Disc") {
+    } else {
         document.getElementById('shapeSelect').style.display = 'block';
     }
 }
