9001a608e9c1587927f7af7c40a859c698558d95
gariero <ceneise@live.ru>
2023-01-13 02:13:12 +0700

    12

--- js/tweaks.js
+++ js/tweaks.js
@@ -1,9 +1,11 @@
-document.getElementById("shapeSelect").addEventListener("change", function () {
-    var selectedOption = this.value;
-    var diameterWidthField = document.getElementById("typesDroplist");
-    if (selectedOption === "Round bar" || selectedOption === "Disc") {
-        diameterWidthField.disabled = true;
-    } else {
-        diameterWidthField.disabled = false;
-    }
+$(document).ready(function () {
+
+    $('#typesDroplist').change(function () {
+        if ($(this).val() == 1) {
+            $('#shapeSelect').prop("disabled", false);
+        } else {
+            $('#shapeSelect').prop("disabled", true);
+        }
+    });
+
 });
